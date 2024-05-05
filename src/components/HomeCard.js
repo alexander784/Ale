09c0b3889a4040d55bc27bc10f6b0a47c91c928e
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 20:14:09 +0300

    Create HomeCard container

---
+++ src/components/HomeCard.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { CardBody, CardItem } from './ui/3DCard';
+
+export function HomeCard() {
+  return (
+    <CardContainer>
+        <CardBody>
+            <CardItem>
+
+            </CardItem>
+        </CardBody>
+    </CardContainer>
+)}
+
+
+export default HomeCard;
