2c91511a95ceee9ceb85b69e47f6c738867aa3aa
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 14:53:35 +0300

    Design NavBar

--- src/components/NavBarLive.js
+++ src/components/NavBarLive.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { cn } from "../utils/cn";
 import { ProductItem } from "./ui/NavBarMenu";
 import { HoveredLink } from "./ui/NavBarMenu";
@@ -11,34 +12,39 @@ export function NavBarLive() {
   );
 }
 
-function Navbar({}) {
+function Navbar({ className }) {
+  const [active, setActive] = useState(null);
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 boder border-red", className)}>
-      <menu>
-        <p className=" text-black dark:text-black text-left sm:text-sm md:text-2xl flex-1 hidden md:flex">Alexander Nyaga</p>
+    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 border border-red", className)}>
+      <menu setActive={setActive}>
+        <p className="text-black dark:text-white text-left sm:text-sm md:text-2xl flex-1 hidden md:flex">Alexander Nyaga</p>
         <p classNam=" text-black dark:text-black text-left sm:text-sm md:text-2xl flex-1 hidden md:flex">MB</p>
-        <menuItem>
-        <div>
-          <HoveredLink>Experiences</HoveredLink>
+        <menuItem setActive={setActive} active={active} item="About me">
+        <div className="flex flex-col space-y-4 text-sm">
+          <HoveredLink href="/interface-design" scrollTo="#experiences">Experiences</HoveredLink>
+        </div>
+        </menuItem>
+        <menuItem setActive={setActive} active={active} item="Projects">
+        <div className="text-sm grid grid-cols-1 md:grid-cols-2 gap-10 p-4">
+          <ProductItem 
+          title="Moringa Pair"
+          href="https://github.com/mit-bhalodi/Resume-Builder"
+          src=""
+          description=""
+          />
+          <ProductItem />
+          <ProductItem />
+          <ProductItem />
         </div>
         </menuItem>
         <menuItem>
-        <div className="">
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
-          <ProductItem />
+        <div className="flex flex-col space-y-4 text-sm">
+          <HoveredLink>LinkedIn</HoveredLink>
+          <HoveredLink>Github</HoveredLink>
+          <HoveredLink>Email</HoveredLink>
         </div>
         </menuItem>
-        <menuItem>
-        <div>
-
-        </div>
-
-        </menuItem>
-
       </menu>
-
     </div>
   )
 
