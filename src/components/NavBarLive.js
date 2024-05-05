f48da8d93ed9fcabe9f654c5bdb3812731fa30bf
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 12:54:16 +0300

    Integrate utility function for class merging and tailwindcss class management

--- src/components/NavBarLive.js
+++ src/components/NavBarLive.js
@@ -1,24 +1,24 @@
-
+import { ProductItem } from "./ui/NavBarMenu";
+import { HoveredLink } from "./ui/NavBarMenu";
 
 
 export function NavBarLive() {
   return (
-    <div className="">
-      <Navbar />
-
+    <div className="relative w-full flex items-center justify-start">
+      <Navbar className="top-2 md:top-4 border rounded-full mx-4 md:mx-auto border-black/[0.2] dark:border-white/[0.2]"/>
     </div>
   );
 }
 
 function Navbar({}) {
   return (
-    <div>
+    <div className={("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50 boder border-red", className)}>
       <menu>
         <p className=" text-black dark:text-black text-left sm:text-sm md:text-2xl flex-1 hidden md:flex">Alexander Nyaga</p>
         <p classNam=" text-black dark:text-black text-left sm:text-sm md:text-2xl flex-1 hidden md:flex">MB</p>
         <menuItem>
         <div>
-          {/* <HoveredLink>Experiences</HoveredLink> */}
+          <HoveredLink>Experiences</HoveredLink>
         </div>
         </menuItem>
         <menuItem>
@@ -31,7 +31,7 @@ function Navbar({}) {
         </menuItem>
         <menuItem>
         <div>
-          
+
         </div>
 
         </menuItem>
