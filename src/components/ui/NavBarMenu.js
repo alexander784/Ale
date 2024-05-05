781a01bd79b20d4a85cca30f97d3cb15de62afdb
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 12:15:11 +0300

    Add smooth scroll functionality with spring transition parametrs

--- src/components/ui/NavBarMenu.js
+++ src/components/ui/NavBarMenu.js
@@ -1,2 +1,21 @@
 import React from 'react';
+import { motion } from "framer-motion";
+
+
+const transition = {
+    type:"spring",
+    mass:0.5,
+    damping:11.5,
+    stiffness:100,
+    restDelta:1.001,
+    restSpeed:1.001,
+};
+
+const scroll = (scrollTo) => {
+    if (scrollTo) {
+        const section = document.querySelector(scrollTo);
+        Selection?.scrollIntoView({ behavior: 'smooth', block: 'start'})
+    }
+};
+
 
