1632dd19c30f6975e153a12473ffc09f7bf2a317
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 20:09:57 +0300

    Create Background component

---
+++ src/components/Background.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export function Background() {
+    return (
+        <div className='h-full w-full dark:bg-gray-700 bg-gray-100 dark:bg-grid-gray/[0.1] bg-grid-black/[0.2] relative flex flex-col overflow-auto'>
+            <div className='h-full w-full flex flex-col items-center'>
+                <div className='py-28 grid grid-col-1 md:grid-cols-2 sm:w-96 md:w-[900px]'>
+                    <div>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
