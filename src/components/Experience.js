69ac5393bddf07a8e1addcb020c50c75d294c51c
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-05 20:31:42 +0300

    Create experince component

---
+++ src/components/Experience.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experience = () => {
+  return (
+    <div className='bg-gray-50 dark:bg-gray-700 p-12 rounded-lg border'>
+
+    </div>
+  )
+}
+
+export default Experience;
