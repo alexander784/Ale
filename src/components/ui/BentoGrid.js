39f035e8862d7fffbae347af2c7f5a4fc4c8ffba
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-06 08:53:17 +0300

    Update Bentogrid component with corrected className assignment

--- src/components/ui/BentoGrid.js
+++ src/components/ui/BentoGrid.js
@@ -1,5 +1,6 @@
 
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 
 export const BentoGrid = ({ className, children }) => {
@@ -14,9 +15,27 @@ export const BentoGrid = ({ className, children }) => {
     );
 };
 
-  return (
-    <div>BentoGrid</div>
-  )
+export const BentoGridItem = ({className, title, description, header,icon, url}) => {
+    return (
+        <Link to={url} target='_blank'
+        className={className(
+            "row-span-1 rounded-x1 group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-gray-700 dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 cursor-pointer",
+            className
 
+        )}
+        >
+            {header}
+            <div className='roup-hover/bento:translate-x-1 transition duration-200 my-1'>
+                {icon}
+                <div className='font-sans font-bold text-neutral-600 dark:text-neutral-200 mb-2 mt-2'>
+                    {title}
+                </div>
+                <div className='font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300'>
+                    {description}
+                </div>
+            </div>
+        </Link>
+    )
+}
 
-export default BentoGrid;
+  
