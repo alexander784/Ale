f7445f9ff9587e853a9ad672b4b9b5f7d82842a2
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-06 18:07:29 +0300

    Create Dashboard component

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Background } from './Background';
+
+const Dashboard = () => {
+  return (
+    <div className='text-black dark:text-white w-full h-full'>
+        <Background />
+
+    </div>
+  )
+}
+
+export default Dashboard;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,13 @@
 import { Background } from "./Background";
+import Dashboard from "./Dashboard";
 import NavBarLive from "./NavBarLive";
 
 const Home = () => {
   return (
     <div>
-        {/* <NavBarLive /> */}
-        <Background />
-        {/* <Dashboard /> */}
+        <NavBarLive />
+        {/* <Background /> */}
+        <Dashboard />
     </div>
   )
 }
