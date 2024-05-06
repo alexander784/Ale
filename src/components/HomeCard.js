b1e37a4e1444461fd31c334611099897a02f4195
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-06 19:16:07 +0300

    Add Home image

--- src/components/HomeCard.js
+++ src/components/HomeCard.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { CardBody, CardItem,CardContainer } from './ui/3DCard';
-
+import cvpic from "../assets/cvpic (1).jpg"
 export function HomeCard() {
   return (
     <CardContainer className="inter-var w-fit bg-gray-100 dark:bg-gray-700/[0.8] z-50">
@@ -10,7 +10,14 @@ export function HomeCard() {
             className="text-xl font-bold text-neutral-600 dark:text-white"
             >
                 Hello, I am Alexander Nyaga
-
+                <img 
+                src={cvpic}
+                height="100"
+                width="100"
+                loading='lazy'
+                className='h-64 w-64 object-cover rounded-xl group-hover/card:shadow-xl'
+                alt="thumbnail"
+                />
             </CardItem>
         </CardBody>
     </CardContainer>
