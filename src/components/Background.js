63dbb697abf76f1143a1771f091eb128727583e2
Alexander784 <alexandernyaga1999@gmail.com>
2024-05-06 18:59:49 +0300

    Design Footer component

--- src/components/Background.js
+++ src/components/Background.js
@@ -2,18 +2,19 @@ import React from 'react';
 import HomeCard from './HomeCard';
 import Experience from './Experience';
 import Projects from './Projects';
+import Footer from './Footer';
 
 
 export function Background() {
     return (
-        <div className='h-full w-full dark:bg-gray-700 bg-gray-100 dark:bg-grid-gray/[0.1] bg-grid-black/[0.2] relative flex flex-col overflow-auto'>
+        <div className='h-full w-full dark:bg-slate-900 bg-slate-900 dark:bg-grid-indigo/[0.1] bg-grid-black/[0.2] relative flex flex-col overflow-auto'>
             <div className='h-full w-full flex flex-col items-center'>
                 <div className='py-28 grid grid-col-1 md:grid-cols-2 sm:w-96 md:w-[900px]'>
                     <div>
                         <HomeCard />
                     </div>
                 <div>
-                <div className='pointer-events-none dark:bg-gray-700 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
+                <div className='pointer-events-none dark:bg-slate-900 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]'></div>
                 <p className='text-4xl sm:text-7xl font-bold py-8 px-10 md:px-0 relative bg-clip-text text-transparent bg-gradient-to-t from-gray-600 via-gray-500 to-gray-700 dark:bg-gradient-to-t dark:from-gray-400 dark:to-white whitespace-normal'>
                 I specialize in crafting dynamic efficient websites.
                 </p>
@@ -27,7 +28,9 @@ export function Background() {
                 <div className="md:w-[900px] p-10" id="myProjects">
                     <p className="text-4xl pb-4 font-bold">Projects</p>
                     <Projects />
+
                 </div>
+                <Footer />
         </div>
     )
 }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,17 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='w-full'>
+        <footer className='bg-white rounded-lg shadow m-4 dark:bg-gray-800'>
+            <div className='w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between'>
+                <span className='text-sm text-gray-500 sm:text-center dark:text-gray-400"'>@ 2024 ALexander Nyaga All rights Reserved.
+
+                </span>
+            </div>
+
+        </footer>
+
+    </div>
   )
 }
 
